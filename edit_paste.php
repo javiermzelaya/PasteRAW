d9f752e3153c23a3c0d22e3499a32e0ba13b0725
Javier <62828021+javiermzelaya@users.noreply.github.com>
2024-08-01 01:34:21 -0300

    Update edit_paste.php

--- edit_paste.php
+++ edit_paste.php
@@ -123,6 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <button type="submit" class="btn btn-primary">Update</button>
     </form>
 </div>
+<?php include 'footbar.php'; ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
