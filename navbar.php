709983bb925b27bc46625a8a7517bcbc05735bb0
Javier <62828021+javiermzelaya@users.noreply.github.com>
2024-08-01 22:33:42 -0300

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -50,7 +50,7 @@ $logo_filename = $settings['logo_filename'] ?? '';
 <script>
     document.addEventListener('DOMContentLoaded', (event) => {
         const toggle = document.getElementById('dark-mode-toggle');
-        const currentTheme = localStorage.getItem('theme') || 'light';
+        const currentTheme = localStorage.getItem('theme') || 'dark';
         const navbar = document.getElementById('navbar');
         const footer = document.getElementById('footer');
 
@@ -90,41 +90,16 @@ $logo_filename = $settings['logo_filename'] ?? '';
     });
 </script>
 <style>
-	.bg-dark {
-    background-color: #1c1c1c !important;
-}
-/* Estilo general del navbar */
-	
-.navbar {
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s ease-in-out;
-}
-
-/* Estilo para el navbar en modo oscuro */
-.navbar-dark .navbar-brand {
-    color: #fff;
-}
-
-.navbar-light .navbar-brand {
-    color: #333;
-}
-
-/* Icono del toggler del navbar */
-.navbar-toggler-icon {
-    background-image: url("data:image/svg+xml;charset=utf-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(0,0,0,.5)' stroke-width='2' d='M4 7h22M4 15h22m-8 8h8'/%3e%3c/svg%3e");
-}
-
-/* Estilo de los enlaces del navbar */
+	/* Estilo de los enlaces del navbar */
 .nav-link {
     text-transform: uppercase; /* Mostrar en mayúsculas */
     transition: color 0.3s ease;
 }
-
-/* Botón prominente */
+	/* Botón prominente */
 .prominent-button {
     font-weight: bold;
-    color: #fff !important; /* Color de texto en modo claro */
-    background-color: #007bff !important; /* Color de fondo en modo claro */
+    color: #999999 !important; /* Color de texto en modo claro */
+    background-color: #ffffff !important; /* Color de fondo en modo claro */
     padding: 5px 10px;
     border-radius: 5px;
     margin-left: 5px; /* Espacio entre los botones */
@@ -132,20 +107,19 @@ $logo_filename = $settings['logo_filename'] ?? '';
 }
 
 .prominent-button:hover {
-    color: #fff;
-    background-color: #0056b3 !important; /* Color de fondo en modo claro al pasar el mouse */
+    color: #999999;
+    background-color: #ffffff !important; /* Color de fondo en modo claro al pasar el mouse */
 }
 
 .dark-mode .prominent-button {
     color: #fff !important; /* Color de texto en modo oscuro */
-    background-color: #0a0a0a !important; /* Color de fondo en modo oscuro */
+    background-color: #1e1e1e !important; /* Color de fondo en modo oscuro */
 }
 
 .dark-mode .prominent-button:hover {
     color: #000;
-    background-color: #18171c !important; /* Color de fondo en modo oscuro al pasar el mouse */
+    background-color: #1e1e1e !important; /* Color de fondo en modo oscuro al pasar el mouse */
 }
-
 /* Estilo del logo del navbar */
 .navbar-logo {
     height: 100px;
@@ -155,26 +129,6 @@ $logo_filename = $settings['logo_filename'] ?? '';
 .navbar-logo:hover {
     transform: scale(1.1);
 }
-
-/* Modo oscuro */
-.dark-mode {
-    background-color: #0a0a0a;
-    color: #ddd;
-}
-
-.dark-mode .navbar {
-    background-color: #0d0d0d; /* Color más oscuro */
-    color: #fff;
-}
-
-.dark-mode .nav-link {
-    color: #ddd;
-}
-
-.dark-mode .nav-link:hover {
-    color: #bb86fc;
-}
-
 /* Responsividad */
 @media (max-width: 768px) {
     .navbar {
